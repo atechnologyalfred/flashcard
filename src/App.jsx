3b894f3a45cfdd3a812ee90db263788b94d03cf1
atechnologyalfred <adjekotaa@gmail.com>
2024-12-25 23:55:27 +0100

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react"
 
-export default function App(){
-return (
-  <>
+const items = [
+  {id: 1, question: 'The property of react that is UI is called what ?', answer: 'JSX', },
+  {id: 2, question: 'The custom component in react is called', answer: 'Props'},
+  {id: 3, question: 'The memory of a react component that is responsible for re-rendering of the entire component is called ?', answer: 'useState'},
+  {id: 4, question: 'The property of react that is UI is called what ?', answer: 'JSX', },
+  {id: 5, question: 'The custom component in react is called', answer: 'Props'},
+  {id: 6, question: 'The memory of a react component that is responsible for re-rendering of the entire component is called ?', answer: 'useState'},
 
-  </>
-)
-
+]
+export default function App () {
+  return (
+    <div id = "container">
+      <h1>The flashcard exercise</h1>
+      <FlashChild/>
+    </div>
+  )
 }
+
+
+
+// function FlashParent ({id, question, answer, isTrue}) {
+//   return (
+//     <li>
+//     <h3>{id}</h3>
+//     <p>{question}</p>
+//     <p>{answer}</p>
+//     </li>
+//   )
+//   }
+
+  function FlashChild () {
+   const [getAnswer, setGetAnswer] = useState(null)
+    function handleChange(id) {
+      setGetAnswer(id)
+    }
+
+    return (
+      <ul className = "flash-card">
+        {items.map((item, itemIndex) => <li key = {itemIndex} onClick={() => handleChange(item.id)}  className = {item.id === getAnswer ? "active" : null }> 
+        {item.id === getAnswer ? item.answer: item.question}</li>
+        )}
+       </ul>
+         
+    )
+    
+  }
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import App from './App.jsx';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 const entryPoint = document.getElementById('root');
-
+import '/index.css';
 ReactDOM.createRoot(entryPoint).render(<App />)
